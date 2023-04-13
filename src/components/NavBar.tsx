04b7d7799bf88f338d609fa1eb4e5cb60fe836ef
Florian <alu.florian@gmail.com>
2023-04-13 19:45:34 +0200

    fix: Navbar not bolding profile tab on profile page

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { type FC, useMemo } from 'react';
+import { type FC } from 'react';
 import Link from 'next/link';
 import { AiOutlineUser, AiOutlineHome } from 'react-icons/ai';
 import logo from '../assets/favicon.svg';
@@ -10,14 +10,6 @@ const NavBar: FC = () => {
     const getCurrentUser = api.users.getCurrent.useQuery();
     const router = useRouter();
 
-    const links = useMemo(
-        () => [
-            { href: '/', label: 'Home', icon: AiOutlineHome },
-            { href: `/${getCurrentUser.data?.user.username || 'profile'}`, label: 'Profile', icon: AiOutlineUser },
-        ],
-        [getCurrentUser.data?.user.username]
-    );
-
     return (
         <nav className='mt-6 flex-1 text-xl'>
             <div className='flex justify-center'>
@@ -28,17 +20,32 @@ const NavBar: FC = () => {
                     >
                         <Image src={logo as StaticImageData} alt='Main Logo' width={36} />
                     </Link>
-                    {links.map(link => (
-                        <li key={link.href}>
-                            <Link
-                                href={link.href}
-                                className='my-0.5 flex w-fit items-center rounded-full py-3 pl-4 pr-5 transition-colors duration-200 hover:bg-gray-200'
+                    <li>
+                        <Link
+                            href='/'
+                            className='my-0.5 flex w-fit items-center rounded-full py-3 pl-4 pr-5 transition-colors duration-200 hover:bg-gray-200'
+                        >
+                            <AiOutlineHome className='mr-4 h-7 w-7' />
+                            <p className={`${router.route === '/' ? 'font-bold' : ''}`}>Home</p>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            href={`/${getCurrentUser.data?.username || ''}`}
+                            className='my-0.5 flex w-fit items-center rounded-full py-3 pl-4 pr-5 transition-colors duration-200 hover:bg-gray-200'
+                        >
+                            <AiOutlineUser className='mr-4 h-7 w-7' />
+                            <p
+                                className={`${
+                                    router.query.username && router.query.username === getCurrentUser.data?.username
+                                        ? 'font-bold'
+                                        : ''
+                                }`}
                             >
-                                <link.icon className='mr-4 h-7 w-7' />
-                                <p className={`${router.route === link.href ? 'font-bold' : ''}`}>{link.label}</p>
-                            </Link>
-                        </li>
-                    ))}
+                                Profile
+                            </p>
+                        </Link>
+                    </li>
                 </ul>
             </div>
         </nav>
