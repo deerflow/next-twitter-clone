c81591efda95e3d8e6e37bf88aab9b7ad20179ee
Florian <alu.florian@gmail.com>
2023-04-13 19:45:10 +0200

    feat: Add button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,21 @@
+import { type FCChildren } from 'deerflow-utils/types';
+import { type HTMLAttributes } from 'react';
+
+const Button: FCChildren<Props> = ({ children, black, ...props }) => {
+    return (
+        <button
+            className={`rounded-full border-[1px] border-solid border-gray-300 px-4 py-1.5 font-semibold ${
+                black ? 'bg-black text-white' : ''
+            }`}
+            {...props}
+        >
+            {children}
+        </button>
+    );
+};
+
+interface Props extends HTMLAttributes<HTMLButtonElement> {
+    black?: boolean;
+}
+
+export default Button;
