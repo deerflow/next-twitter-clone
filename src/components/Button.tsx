2697fff34135a18f66d2483070cc990e0db0607c
Florian <alu.florian@gmail.com>
2023-04-15 21:20:15 +0200

    feat: Add hover effects on Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,8 +4,8 @@ import { type ButtonHTMLAttributes } from 'react';
 const Button: FCChildren<Props> = ({ children, black, ...props }) => {
     return (
         <button
-            className={`rounded-full border-[1px] border-solid border-gray-300 px-4 py-1.5 font-semibold ${
-                black ? 'bg-black text-white' : ''
+            className={`rounded-full border-[1px] border-solid border-gray-300 px-4 py-1.5 font-semibold transition-colors duration-200 ${
+                black ? 'bg-black text-white hover:bg-gray-800' : 'hover:bg-gray-200'
             }`}
             {...props}
         >
