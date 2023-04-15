c3db556900023cdf7630c4aeefaf537ffaf7621e
Florian <alu.florian@gmail.com>
2023-04-15 21:23:36 +0200

    fix: Wrong return type for CommentsList

--- src/components/CommentsList.tsx
+++ src/components/CommentsList.tsx
@@ -17,62 +17,62 @@ const CommentsList: FC<Props> = ({ comments }) => {
     if (!comments) return null;
 
     return (
-        comments &&
-        comments.length > 0 && (
-            <div>
-                {comments.map(comment => (
-                    <div
-                        key={comment.id}
-                        className='flex flex-col break-words border-b-[1px] border-solid border-gray-200 p-4 transition-colors duration-200 hover:bg-gray-100'
-                    >
-                        <div className='mb-3 flex'>
-                            <div className='flex w-full'>
-                                <Link href={`/${comment.authorId}`}>
-                                    <Image
-                                        src={comment.author.avatar}
-                                        alt='Default user image'
-                                        width={48}
-                                        height={48}
-                                        className='mr-3 h-12 w-12 rounded-full object-cover object-center'
-                                    />
-                                </Link>
-
-                                <div className='w-[512px] min-w-0'>
-                                    <Link
-                                        href={`/${comment.author.username}`}
-                                        className='break-words text-lg font-semibold hover:underline'
-                                    >
-                                        {comment.author?.username}
-                                    </Link>
-                                    <p className='whitespace-pre-line break-words'>{comment.content}</p>
-                                </div>
-                            </div>
-                            {user && user.data?.id === comment.author.id && (
-                                <button>
-                                    {!deleteComment.isLoading ? (
-                                        <AiOutlineDelete
-                                            className='h-5 w-5'
-                                            onClick={e => {
-                                                e.stopPropagation();
-                                                void deleteComment.mutate(
-                                                    { id: comment.id },
-                                                    {
-                                                        onSuccess: () => {
-                                                            void context.comments.getAllForPost.invalidate({
-                                                                postId: comment.postId,
-                                                            });
-                                                        },
-                                                    }
-                                                );
-                                            }}
+        <>
+            {comments && comments.length > 0 && (
+                <div>
+                    {comments.map(comment => (
+                        <div
+                            key={comment.id}
+                            className='flex flex-col break-words border-b-[1px] border-solid border-gray-200 p-4 transition-colors duration-200 hover:bg-gray-100'
+                        >
+                            <div className='mb-3 flex'>
+                                <div className='flex w-full'>
+                                    <Link href={`/${comment.authorId}`}>
+                                        <Image
+                                            src={comment.author.avatar}
+                                            alt='Default user image'
+                                            width={48}
+                                            height={48}
+                                            className='mr-3 h-12 w-12 rounded-full object-cover object-center'
                                         />
-                                    ) : (
-                                        <Spinner size={5} />
-                                    )}
-                                </button>
-                            )}
-                        </div>
-                        {/*post.image && (
+                                    </Link>
+
+                                    <div className='w-[512px] min-w-0'>
+                                        <Link
+                                            href={`/${comment.author.username}`}
+                                            className='break-words text-lg font-semibold hover:underline'
+                                        >
+                                            {comment.author?.username}
+                                        </Link>
+                                        <p className='whitespace-pre-line break-words'>{comment.content}</p>
+                                    </div>
+                                </div>
+                                {user && user.data?.id === comment.author.id && (
+                                    <button>
+                                        {!deleteComment.isLoading ? (
+                                            <AiOutlineDelete
+                                                className='h-5 w-5'
+                                                onClick={e => {
+                                                    e.stopPropagation();
+                                                    void deleteComment.mutate(
+                                                        { id: comment.id },
+                                                        {
+                                                            onSuccess: () => {
+                                                                void context.comments.getAllForPost.invalidate({
+                                                                    postId: comment.postId,
+                                                                });
+                                                            },
+                                                        }
+                                                    );
+                                                }}
+                                            />
+                                        ) : (
+                                            <Spinner size={5} />
+                                        )}
+                                    </button>
+                                )}
+                            </div>
+                            {/*post.image && (
                         <NextImage
                             alt='Uploaded image'
                             src={post.image.url}
@@ -82,10 +82,11 @@ const CommentsList: FC<Props> = ({ comments }) => {
                             placeholder='blur'
                         />
                     )*/}
-                    </div>
-                ))}
-            </div>
-        )
+                        </div>
+                    ))}
+                </div>
+            )}
+        </>
     );
 };
 
